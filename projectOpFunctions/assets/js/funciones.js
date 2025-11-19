1d48a9d7dfda062830adacb7b392d4431cc6987e
castro08 <miguelangelcastro200400@gmail.com>
2025-11-19 13:10:42 -0600

    avances fundamentos js

--- projectOpFunctions/assets/js/funciones.js
+++ projectOpFunctions/assets/js/funciones.js
@@ -2,6 +2,32 @@
  * funciones
  */
 function myFunction () {
-    document.getElementById ("demo3").innerHTML="Emile Rengifo ";
+    document.getElementById ("demo3").innerHTML="Miguel Castro ";
     
+}
+/** FUNCION PARA capturar los datos */
+
+function datos(){
+    let titulo= document.getElementById("titulo").value;
+    let autor= document.getElementById("autor").value;
+    let resul= unir(titulo,autor);
+    document.getElementById("msj").innerHTML=resul;
+
+
+
+}
+/**funtion para unir una cadena de tezxto */
+
+function unir(titulo,autor){
+return"titulo" + titulo + "</br>" + "autor"+autor
+}
+/** EJEMPLO CON EL OBEJTO MATH */
+
+function redondearNumero(){
+    let num = document.getElementById("numero").value;
+     /** convertir a numero y deondear */
+    let redondeo=Math.round(Number(num));
+
+     document.getElementById("resul").innerHTML=
+     "el numero redondeado es" + redondeo;
 }
